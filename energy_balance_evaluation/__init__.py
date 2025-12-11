08c7cdec8d465d33a2ff426af575d0114b1be5a7
Max Nutz <max.nutz@boku.ac.at>
2025-12-11 12:27:19 +0100

    remove setup.py for pyproj.toml using corresponding __init__ - structure

---
+++ energy_balance_evaluation/__init__.py
@@ -0,0 +1,8 @@
+from energy_balance_eval import (EnergyBalanceEval)
+from pypsa_network_eval import (CarrierNetwork, eval_all_networks)
+
+__all__ = [
+    "EnergyBalanceEval",
+    "CarrierNetwork",
+    "eval_all_networks",
+]
