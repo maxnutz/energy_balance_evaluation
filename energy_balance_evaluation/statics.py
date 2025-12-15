8c355ad0b07a15e1fb93b4b28c44c8017ff4f4f2
Max Nutz <max.nutz@boku.ac.at>
2025-12-15 17:55:37 +0100

    close consistency issues with pypsa-at code

--- energy_balance_evaluation/statics.py
+++ energy_balance_evaluation/statics.py
@@ -1,119 +1,121 @@
 # dict of rows to include for the light version of the energy balance
 rows_to_include_dict = {
-    "Total absolute values": {
-        "Primary production": True,  #
-        "Recovered & recycled products": False,
-        "Imports": True,  #
-        "Exports": True,  #
-        "Change in stock": True,  #
-        "Gross available energy": True,  #
-        "International maritime bunkers": True,  #
-        "Gross inland consumption": True,  #
-        "International aviation": True,  #
-        "Total energy supply": True,  #
-        "Gross inland consumption (Europe 2020-2030)": False,
-        "Primary energy consumption (Europe 2020-2030)": False,
-        "Final energy consumption (Europe 2020-2030)": False,
+    "Total_absolute_values": {
+        "Primary_production": True,
+        "Recovered_and_recycled_products": False,
+        "Imports": True,
+        "Exports": True,
+        "Change_in_stock": True,
+        "Gross_available_energy": True,
+        "International_maritime_bunkers": True,
+        "Gross_inland_consumption": True,
+        "International_aviation": True,
+        "Total_energy_supply": True,
+        "Gross_inland_consumption_(Europe_2020-2030)": False,
+        "Primary_energy_consumption_(Europe_2020-2030)": False,
+        "Final_energy_consumption_(Europe_2020-2030)": False,
     },
-    "Transformation input": {
+    "Transformation_input": {
         "nan": True,
-        "Electricity & heat generation": {
+        "Electricity_and_heat_generation": {
             "nan": True,
-            "Main activity producer electricity only": True,
-            "Main activity producer CHP": True,
-            "Main activity producer heat only": True,
-            "Autoproducer electricity only": True,  # sum to Main activitiv producer electricity only
-            "Autoproducer CHP ": True,  # sum to Main activity producer CHP
-            "Autoproducer heat only": True,  # sum to Main activity producer heat only
-            "Electrically driven heat pumps": True,
-            "Electric boilers": False,
-            "Electricity for pumped storage": True,
-            "Derived heat for electricity production": False,
+            "Main_activity_producer_electricity_only": True,
+            "Main_activity_producer_CHP": True,
+            "Main_activity_producer_heat_only": True,
+            "Autoproducer_electricity_only": True,
+            "Autoproducer_CHP_": True,
+            "Autoproducer_heat_only": True,
+            "Electrically_driven_heat_pumps": True,
+            "Electric_boilers": False,
+            "Electricity_for_pumped_storage": True,
+            "Derived_heat_for_electricity_production": False,
         },
-        "Coke ovens": False,
-        "Blast furnaces": False,
-        "Gas works": True,
-        "Refineries & petrochemical industry": {
+        "Coke_ovens": False,
+        "Blast_furnaces": False,
+        "Gas_works": True,
+        "Refineries_and_petrochemical_industry": {
             "nan": True,
-            "Refinery intake": False,
-            "Backflows from petrochemical industry": False,
-            "Products transferred": False,
-            "Interproduct transfers": False,
-            "Direct use": False,
-            "Petrochemical industry intake": False,
+            "Refinery_intake": False,
+            "Backflows_from_petrochemical_industry": False,
+            "Products_transferred": False,
+            "Interproduct_transfers": False,
+            "Direct_use": False,
+            "Petrochemical_industry_intake": False,
         },
-        "Patent fuel plants": False,
-        "BKB & PB plants": False,
-        "Coal liquefaction plants": False,
-        "For blended natural gas": False,
-        "Liquid biofuels blended": False,
-        "Charcoal production plants": False,
-        "Gas-to-liquids plants": False,
-        "Not elsewhere specified ": False,
+        "Patent_fuel_plants": False,
+        "BKB_and_PB_plants": False,
+        "Coal_liquefaction_plants": False,
+        "For_blended_natural_gas": False,
+        "Liquid_biofuels_blended": False,
+        "Charcoal_production_plants": False,
+        "Gas-to-liquids_plants": False,
+        "Not_elsewhere_specified_": False,
     },
-    "Transformation output": False,
-    "Energy sector": False,
-    "Distribution losses": True,
-    "Available for final consumption": True,
-    "Final non-energy consumption": False,
-    "Final energy consumption": {
+    "Transformation_output": False,
+    "Energy_sector": False,
+    "Distribution_losses": True,
+    "Available_for_final_consumption": True,
+    "Final_non-energy_consumption": False,
+    "Final_energy_consumption": {
         "nan": True,
-        "Industry sector": {
+        "Industry_sector": {
             "nan": True,
-            "Iron & steel": False,
-            "Chemical & petrochemical": False,
-            "Non-ferrous metals": False,
-            "Non-metallic minerals": False,
-            "Transport equipment": False,
+            "Iron_and_steel": False,
+            "Chemical_and_petrochemical": False,
+            "Non-ferrous_metals": False,
+            "Non-metallic_minerals": False,
+            "Transport_equipment": False,
             "Machinery": False,
-            "Mining & quarrying": False,
-            "Food, beverages & tobacco": False,
-            "Paper, pulp & printing": False,
-            "Wood & wood products": False,
+            "Mining_and_quarrying": False,
+            "Food_beverages_and_tobacco": False,
+            "Paper_pulp_and_printing": False,
+            "Wood_and_wood_products": False,
             "Construction": False,
-            "Textile & leather": False,
-            "Not elsewhere specified (industry)": False,
+            "Textile_and_leather": False,
+            "Not_elsewhere_specified_(industry)": False,
         },
-        "Transport sector": {
+        "Transport_sector": {
             "nan": True,
-            "Rail": True,  # sum with road
-            "Road": True,  # sum with Rail
-            "Domestic aviation": True,  # sum with international aviation!
-            "Domestic navigation": True,  # sum with Road and Rail
-            "Pipeline transport": True,
-            "Not elsewhere specified (transport)": False,
+            "Rail": True,
+            "Road": True,
+            "Domestic_aviation": True,
+            "Domestic_navigation": True,
+            "Pipeline_transport": True,
+            "Not_elsewhere_specified_(transport)": False,
         },
-        "Other sectors": {
+        "Other_sectors": {
             "nan": True,
-            "Commercial & public services": True,  # sum with households
-            "Households": True,  # sum with commercial & public services
-            "Agriculture & forestry": True,
-            "Fishing": True,  # sum with agriculture & forestry??
-            "Not elsewhere specified (other)": False,
+            "Commercial_and_public_services": True,
+            "Households": True,
+            "Agriculture_and_forestry": True,
+            "Fishing": True,
+            "Not_elsewhere_specified_(other)": False,
         },
     },
 }
 
+
 # dict of rows-lists to be added together for the light version of the energy balance
 rows_to_add_dict = {
-    "Final energy consumption>Transport sector>Land transport": [
-        "Final energy consumption>Transport sector>Road",
-        "Final energy consumption>Transport sector>Rail",
-        "Final energy consumption>Transport sector>Domestic navigation",
+    "Final_energy_consumption>Transport_sector>Land_transport": [
+        "Final_energy_consumption>Transport_sector>Road",
+        "Final_energy_consumption>Transport_sector>Rail",
+        "Final_energy_consumption>Transport_sector>Domestic_navigation",
     ],
-    "Total absolute values>National and International aviation": [
-        "Total absolute values>International aviation",
-        "Final energy consumption>Transport sector>Domestic aviation",
+    "Total_absolute_values>National_and_International_aviation": [
+        "Total_absolute_values>International_aviation",
+        "Final_energy_consumption>Transport_sector-Domestic_aviation",
     ],
-    "Final energy consumption>Other sectors>Households & Commercial & public services": [
-        "Final energy consumption>Other sectors>Commercial & public services",
-        "Final energy consumption>Other sectors>Households",
+    "Final_energy_consumption>Other_sectors>Households_and_Commercial_and_public_services": [
+        "Final_energy_consumption>Other_sectors>Commercial_and_public_services",
+        "Final_energy_consumption>Other_sectors>Households",
     ],
-    "Final energy consumption>Other sectors>Agriculture & forestry & fishing": [
-        "Final energy consumption>Other sectors>Agriculture & forestry",
-        "Final energy consumption>Other sectors>Fishing",
+    "Final_energy_consumption>Other_sectors>Agriculture_and_forestry_and_fishing": [
+        "Final_energy_consumption>Other_sectors>Agriculture_and_forestry",
+        "Final_energy_consumption>Other_sectors>Fishing",
     ],
 }
+
 
 # dict of string elements to be replaced when creating light energy balance names
 eb_row_string_replacement_dict = {" ": "_", ",": "_", "&": "and", "/": "_"}
