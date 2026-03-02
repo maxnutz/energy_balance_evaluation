815cac26d988b4c7f805a65a5e96b83417ec89e6
Max Nutz <max.nutz@boku.ac.at>
2026-03-02 16:29:26 +0100

    black formatter

--- energy_balance_evaluation/_helpers.py
+++ energy_balance_evaluation/_helpers.py
@@ -16,14 +16,15 @@ def process_pypsa_mapping_to_csv(
         Name of the excel sheet to read from the pypsa mapping file.
         Defaults to "20251114_inputs_eb_pypsa".
     """
-    pd.read_excel(mapping_filepath, sheet_name=sheet_name, engine = "odf").to_csv(
-        mapping_filepath.replace(".ods", ".csv"), sep= ";",index=False
+    pd.read_excel(mapping_filepath, sheet_name=sheet_name, engine="odf").to_csv(
+        mapping_filepath.replace(".ods", ".csv"), sep=";", index=False
     )
 
 
 def main():
     print("Directly executing _helpers-file!")
     process_pypsa_mapping_to_csv("~/data/AT-balance-sheets-April2025-mapped.ods")
+
 
 if __name__ == "__main__":
     main()
