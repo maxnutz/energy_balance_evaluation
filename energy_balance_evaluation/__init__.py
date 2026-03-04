928d9600fec3c4aadd78f19c230aa28985170e22
Max Nutz <max.nutz@boku.ac.at>
2026-03-04 14:50:12 +0100

    MAN: delete code parts not needed for flat energy balance evaluation

--- energy_balance_evaluation/__init__.py
+++ energy_balance_evaluation/__init__.py
@@ -1,38 +1,15 @@
 """
-This package contains a set of tools for evaluating energy balances and pypsa networks.
+This package contains a set of tools for
+- evaluating flat energy balance
+- creating structured variable definitions and codelists for
+  Energy System Model Validation
+- validate ESM model outputs  in IAMC-format vs Eurostat Energy Balance values
 
-The following classes and functions are available:
-
-- `EnergyBalance`: Class for evaluating the energy balance data
-- `CarrierNetwork`: Class for evaluating the pypsa-network per carrier
-- `eval_all_networks`: Function for evaluating given network for all carriers in the respective network
-- `extract_true_keys`: Function for extracting keys from a dictionary where the value is True
-- `rows_to_include_dict`: Dictionary of rows to include for the light version of the energy balance
-- `rows_to_add_dict`: Dictionary of rows to add together for the light version of the energy balance
-- `non_numerical_columns_list`: List of non-numerical columns in eurostat energy balance matrix
 """
 
 # from energy_balance_evaluation.energy_balance_eval import EnergyBalance
-from energy_balance_evaluation.pypsa_network_eval import (
-    CarrierNetwork,
-    eval_all_networks,
-)
-from energy_balance_evaluation.utils import extract_true_keys, read_mapping_csv
-from energy_balance_evaluation.statics import (
-    rows_to_add_dict,
-    rows_to_include_dict,
-    non_numerical_columns_list,
-    eb_row_string_replacement_dict,
-)
+from energy_balance_evaluation.energy_balance_eval import VariablesSet
 
 __all__ = [
-    # "EnergyBalance",
-    "CarrierNetwork",
-    "eval_all_networks",
-    "extract_true_keys",
-    "read_mapping_csv",
-    "rows_to_include_dict",
-    "rows_to_add_dict",
-    "non_numerical_columns_list",
-    "eb_row_string_replacement_dict",
+    "VariablesSet",
 ]
